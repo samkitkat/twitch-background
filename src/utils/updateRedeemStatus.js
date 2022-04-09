c7cd82408f89ff13508d6a0be4eac968fcf12959
Samar Barakat <samar@Samars-MacBook-Pro.local>
2022-04-09 14:51:53 -0700

    fix: updateRedeemStatus

--- src/utils/updateRedeemStatus.js
+++ src/utils/updateRedeemStatus.js
@@ -3,7 +3,7 @@ export const status = {
     fulfilled: "FULFILLED"
 }
 
-export const updateRedeemStatus = (client_id, auth_token, user_id, reward_id, redeem_id, status) => {
+export const updateRedeemStatus = (client_id, user_id, reward_id, redeem_id, status) => {
     fetch(`https://api.twitch.tv/helix/channel_points/custom_rewards/redemptions?broadcaster_id=${user_id}&reward_id=${reward_id}&id=${redeem_id}}`, {
         method: 'PATCH',
         body: JSON.stringify({ "status": status }),
